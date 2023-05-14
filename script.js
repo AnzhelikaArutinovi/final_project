88818573150ad4708de992900bf1c693f35e0f37
AnzhelikaArutinovi <anzhelika.arutinovi.1@btu.edu.ge>
2023-05-15 01:19:07 +0400

    accordion created

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 "use strict";
+// Burger logic
+let burgerWraper = document.querySelector(".burger-wraper");
 
-let headerBar = document.querySelector(".burger-wraper");
+burgerWraper.addEventListener("click", function () {
+	let divOverlay = document.querySelector(".overlay");
+	let headerWraper = document.querySelector(".header-wraper");
+	let burgerLines = document.querySelectorAll(".burger-line");
+	let headerNav = document.querySelector(".navigation");
+	let headerBtns = document.querySelector(".header-buttons");
 
-headerBar.addEventListener("click", function () {
-  let divOverlay = document.querySelector(".overlay");
-  let headerWraper = document.querySelector(".header-wraper");
-  let burgerLines = document.querySelectorAll(".burger-line");
-  let burgerWraper = document.querySelector(".burger-wraper");
-  let headerNav = document.querySelector(".navigation");
-  let headerBtns = document.querySelector(".header-buttons");
+	divOverlay.classList.toggle("visible");
+	headerWraper.classList.toggle("visible");
+	burgerLines.forEach((line) => {
+		line.classList.toggle("visible");
+	});
+	burgerWraper.classList.toggle("visible");
+	headerNav.classList.toggle("hidden");
+	headerBtns.classList.toggle("hidden");
+	document.body.style.overflow = "hidden";
+});
 
-  divOverlay.classList.toggle("visible");
-  headerWraper.classList.toggle("visible");
-  burgerLines.forEach((line) => {
-    line.classList.toggle("visible");
-  });
-  burgerWraper.classList.toggle("visible");
-  headerNav.classList.toggle("hidden");
-  headerBtns.classList.toggle("hidden");
-  document.body.style.overflow = "hidden";
+// Accordion
+let accordionContainer = document.querySelectorAll(".question-container");
+
+accordionContainer.forEach(function (item) {
+	item.addEventListener("click", function () {
+		this.classList.toggle("active");
+	});
 });
